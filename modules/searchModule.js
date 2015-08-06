d6d8bd8068fe669820efa2b156c2ed7bfc805f12
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2015-08-06 18:41:24 +0200

    added list section with fade animation

--- modules/searchModule.js
+++ modules/searchModule.js
@@ -6,12 +6,15 @@
         return (window.innerWidth-$(".info-section").width())/2;
     }
 
+    var showItems = false;
+
     searchModule.controller("SearchCtrl", ["$scope", "$http", function($scope, $http) {
         $(window).resize(function(){
             $(".info-section").css("margin-left", ($scope.moveInfoToLeft ? 0 : getProperSectionMargin()+"px"));
         });
 
-        $scope.moveInfoToLeft = false;
+        $scope.showItems = showItems;
+        $scope.searchBar = "";
 
         $scope.checkSearchBar = function() {
             //console.log($scope.searchBar);
@@ -19,13 +22,14 @@
 
         $scope.setSearchBarFocus = function(focused) {
             if(!focused)
-                $scope.moveInfoToLeft = $scope.searchBar != "";
+                showItems = $scope.searchBar != "";
             else
             {
                 if($scope.searchBar == "")
-                    $scope.moveInfoToLeft = false;
+                    showItems = false;
             }
-            $(".info-section").css("margin-left", ($scope.moveInfoToLeft ? 0 : getProperSectionMargin()+"px"));
+            $(".info-section").css("margin-left", (showItems ? 0 : getProperSectionMargin()+"px"));
+            $scope.showItems = showItems;
         };
     }]);
 })();
