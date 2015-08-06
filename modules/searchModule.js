4ac267b61ff386509b9a8bba114a33eb5aca1529
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2015-08-06 17:47:59 +0200

    added section animation, updated margin

--- modules/searchModule.js
+++ modules/searchModule.js
@@ -1,7 +1,31 @@
 (function() {
     var searchModule = angular.module("SearchModule", []);
 
+    function getProperSectionMargin()
+    {
+        return (window.innerWidth-$(".info-section").width())/2;
+    }
+
     searchModule.controller("SearchCtrl", ["$scope", "$http", function($scope, $http) {
-        $scope.focus = false;
+        $(window).resize(function(){
+            $(".info-section").css("margin-left", ($scope.moveInfoToLeft ? 0 : getProperLeftMargin()+"px"));
+        });
+
+        $scope.moveInfoToLeft = false;
+
+        $scope.checkSearchBar = function() {
+            //console.log($scope.searchBar);
+        };
+
+        $scope.setSearchBarFocus = function(focused) {
+            if(!focused)
+                $scope.moveInfoToLeft = $scope.searchBar != "";
+            else
+            {
+                if($scope.searchBar == "")
+                    $scope.moveInfoToLeft = false;
+            }
+            $(".info-section").css("margin-left", ($scope.moveInfoToLeft ? 0 : getProperSectionMargin()+"px"));
+        };
     }]);
 })();
